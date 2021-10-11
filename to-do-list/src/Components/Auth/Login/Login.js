c5977bdd0b8c5533bb62a0ff9d7df48d2e563e00
kyrylpopyk <kyrylpopyk@gmail.com>
2021-10-11 03:33:05 +0200

    Refactoring

--- to-do-list/src/Components/Auth/Login/Login.js
+++ to-do-list/src/Components/Auth/Login/Login.js
@@ -2,6 +2,28 @@ import AuthForm from "../../Library/AuthForm.style.js";
 import style from "./Login.module.css";
 import {NavLink} from "react-router-dom";
 
+const LoginForm = () => {
+    return(
+        <form className={style.loginForm}>
+            <div>
+                <input type="text" placeholder="Email or Username"></input>
+            </div>
+            <div>
+                <input type="text" placeholder="Password"></input>
+            </div>
+            <div>
+                <button className={style.loginBtn}>Login</button>
+            </div>
+            <div>
+                <span className={style.additional}>or</span>
+            </div>
+            <div>
+                <NavLink to="/auth/register" className={style.loginCreate}>Create an account</NavLink>
+            </div>
+        </form>
+    );
+}
+
 const Login = () => {
     return(
         <AuthForm
@@ -10,23 +32,7 @@ const Login = () => {
             rows="3fr 8fr">
 
             <div className={style.loginTitle}>Login</div>
-            <form className={style.loginForm}>
-                <div>
-                    <input type="text" placeholder="Email or Username"></input>
-                </div>
-                <div>
-                    <input type="text" placeholder="Password"></input>
-                </div>
-                <div>
-                    <button type="submit" className={style.loginBtn}>Login</button>
-                </div>
-                <div>
-                    <span className={style.additional}>or</span>
-                </div>
-                <div>
-                    <NavLink to="/auth/register" className={style.loginCreate}>Create an account</NavLink>
-                </div>
-            </form>
+            <LoginForm />
         </AuthForm>
     );
 }
